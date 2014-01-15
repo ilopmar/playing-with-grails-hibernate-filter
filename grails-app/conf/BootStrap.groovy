b3b561c7c9e32fcb6a28abb743d0aa706af4e098
Iván López <lopez.ivan@gmail.com>
2014-01-15 23:22:51 +0100

    Test data created during app start

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,23 @@
+import example.User
+import example.Sport
+import example.UserSport
+
 class BootStrap {
 
     def init = { servletContext ->
+        this.loadData()
     }
+
     def destroy = {
+    }
+
+    private void loadData() {
+        def u1 = new User(name: 'user1', enabled: true, isAdmin: false).save(failOnError: true)
+        def s1 = new Sport(name: 'ski').save(failOnError: true)
+        def us1 = new UserSport(user: u1, sport: s1, trainingPlace: 'my place').save(failOnError: true)
+
+        def u2 = new User(name: 'user2', enabled: true, isAdmin: true).save(failOnError: true)
+        def u3 = new User(name: 'user3', enabled: false, isAdmin: false).save(failOnError: true)
+        def u4 = new User(name: 'user4', enabled: false, isAdmin: true).save(failOnError: true)
     }
 }
